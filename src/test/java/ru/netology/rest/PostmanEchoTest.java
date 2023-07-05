8522a30f0e577f0476cdeee86cd6aa5bc1bf0070
Liza Stepanova <tuchafeb@mail.ru>
2023-07-05 21:30:11 +0300

    git commit -m "test failed with Yello"

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -21,7 +21,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hello"));
+                .body("data", equalTo("Yello"));
     }
 }
 
